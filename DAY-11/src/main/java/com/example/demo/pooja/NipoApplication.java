d963c01ae4ff5e70add6be41275505dd20192356
Dhayanandapathy <dhayanandapathy@gmail.com>
2023-09-27 09:41:44 +0530

    commit

---
+++ DAY-11/src/main/java/com/example/demo/pooja/NipoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.pooja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NipoApplication.class, args);
+	}
+
+}
---
+++ DAY-11/src/test/java/com/example/demo/pooja/NipoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.pooja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NipoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
